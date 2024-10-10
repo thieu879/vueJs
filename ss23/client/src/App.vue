955996941af2b06e80c95fae529d0be33cfcb599
thieu879 <nguyenthieu11021995@gmail.com>
2024-10-11 03:20:49 +0700

    abc

---
+++ ss23/client/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <ListStudent></ListStudent>
+  </div>
+</template>
+
+<script setup>
+import ListStudent from './components/ListStudent.vue';
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
